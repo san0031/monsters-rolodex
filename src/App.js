421d8b8b8cefe91e00a32e7725bf55c146afcba0
Srividya Ananthasubramoney <san0031@gmail.com>
2022-07-11 09:44:05 -0500

    Upto lesson 29

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
+import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class  App extends Component {
+constructor(){
+  super();
+  this.state ={
+    name: 'Vids'
+  }
+}
+  render() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello my name is Vidya
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <button onClick={() => {
+        this.setState({ name: 'Money'});
+        console.log(this.state);
+       }}>
+        Change {this.state.name}       </button>
       </header>
     </div>
   );
 }
-
+}
 export default App;
