f29017826054cf7c4ed03d38eaa71a70f0368a94
Srividya Ananthasubramoney <san0031@gmail.com>
2022-07-11 11:06:51 -0500

    Upto lesson 30

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ class  App extends Component {
 constructor(){
   super();
   this.state ={
-    name: 'Vids'
-  }
+    name: {firstName:'Vidya', lastName: 'Money'},
+    company: 'FIS',
+  };
 }
   render() {
   return (
@@ -15,13 +16,13 @@ constructor(){
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello my name is Vidya
+          Hello my name is  {this.state.name.firstName} {this.state.name.lastName}, I work at {this.state.company}
         </p>
        <button onClick={() => {
-        this.setState({ name: 'Money'});
-        console.log(this.state);
+        this.setState({ name: {firstName: 'Sri' , lastName: 'Ananthasubramoney'}});
+        console.log(this.state)
        }}>
-        Change {this.state.name}       </button>
+        Change {this.state.name.lastName}       </button>
       </header>
     </div>
   );
