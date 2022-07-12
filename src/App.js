8044c22f22f0b7e2131a55d43afdce8500fdba73
Srividya Ananthasubramoney <san0031@gmail.com>
2022-07-11 23:29:25 -0500

    Upto lesson 45

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CardList from './components/card-list/card-list.component';
 
 class  App extends Component {
 constructor(){
@@ -32,10 +33,11 @@ onSearchChange=  (event) => {
   return (
     <div className="App">
       <input className='search-box' type='search' placeholder='search monsters'  onChange={onSearchChange}/>
-     {  filteredMonsters.map((monster) => {
+    {/* {  filteredMonsters.map((monster) => {
         return <div  key={monster.id}><h1>{monster.name}</h1></div>;
      }
-     )}
+     )}*/}
+     <CardList></CardList>
 </div>
   )}
 
